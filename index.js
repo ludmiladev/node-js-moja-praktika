226aca85c40d60c0351d55e49242839c4280dd7a
ludmiladev <ljuda12@gmail.com>
2021-03-10 06:12:44 +0200

    Задача 2.2  Модифицируйте предыдущую задачу так, в файле-модуле хранилось 3 функции: возводящая в квадрат, возводящая в куб и возводящая в четвертую степень. Экспортируйте все эти функции в файле index.js и с их помощью возведите числа с разные степени.

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 
 let math = require('./math');
 console.log(math.square(4));
+console.log(math.cube(4));
+console.log(math.step(4));
--- math.js
+++ math.js
@@ -1,28 +1,37 @@
 'use strict';
 
 function square(num) {
-    // if (check (num)) {
+    if (check (num)) {
         return num*num ;
-    // } else {
-    //     return null;
-    //    }
+    } else {
+        return null;
+       }
     }
-//
-// function cube(num) {
-//     if (check (num)) {
-//         return num*num*num;
-//     } else {
-//         return null;
-//     }
-// }
-//
-// function check(num) {
-//     if (num>0) {
-// return true
-//     } else {
-// return false;
-//     }
-// }
+
+function cube(num) {
+    if (check(num)) {
+        return num * num * num;
+    } else {
+        return null;
+    }
+}
+
+    function step(num) {
+        if (check (num)) {
+            return num*num*num*num;
+        } else {
+            return null;
+        }
+}
+
+function check(num) {
+    if (num>0) {
+return true
+    } else {
+return false;
+    }
+}
 
 exports.square = square;
-// exports.cube = cube;
+exports.cube = cube;
+exports.step = step;
