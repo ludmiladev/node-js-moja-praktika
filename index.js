346608817374413c5aa841dcbac72f9c3b0125aa
ludmiladev <ljuda12@gmail.com>
2021-03-10 07:21:05 +0200

    синхронное выполнение

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ let text = fs.readFileSync('readme.txt', 'utf8');
 console.log(text);
 
 
+/*
+fs.readFile('readme.txt', 'utf8', function (err, data) {
+    if (err) throw err;
 
+    console.log(data);
+});
+console.log('!!!');
+ */
 
