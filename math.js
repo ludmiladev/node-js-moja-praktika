41bd8c0fb0fa32716132b393b8ba1391b7c0efee
ludmiladev <ljuda12@gmail.com>
2021-03-10 06:04:15 +0200

    Задача 2.1 Сделайте файл-модуль math.js, в котором будет лежать функция square, возводящая число в квадрат. В файле index.js экспортируйте модуль math.js, и с его помощью возведите число в квадрат.

--- math.js
+++ math.js
@@ -1,28 +1,28 @@
 'use strict';
 
 function square(num) {
-    if (check (num)) {
+    // if (check (num)) {
         return num*num ;
-    } else {
-        return null;
-       }
+    // } else {
+    //     return null;
+    //    }
     }
-
-function cube(num) {
-    if (check (num)) {
-        return num*num*num;
-    } else {
-        return null;
-    }
-}
-
-function check(num) {
-    if (num>0) {
-return true
-    } else {
-return false;
-    }
-}
+//
+// function cube(num) {
+//     if (check (num)) {
+//         return num*num*num;
+//     } else {
+//         return null;
+//     }
+// }
+//
+// function check(num) {
+//     if (num>0) {
+// return true
+//     } else {
+// return false;
+//     }
+// }
 
 exports.square = square;
-exports.cube = cube;
+// exports.cube = cube;
