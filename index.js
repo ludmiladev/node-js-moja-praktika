7cf953af2aa2cb4f313de38d8550b803562b4175
ludmiladev <ljuda12@gmail.com>
2021-03-11 03:48:38 +0200

    Задача 3.1  Дан файл с текстом. Откройте его асинхронно, прочитайте текст и выведите его в консоль.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 'use strict';
 
+
 let fs = require('fs');
 
+/*
 let text = fs.readFileSync('readme.txt', 'utf8');
 console.log(text);
+*/
 
-
-/*
-fs.readFile('readme.txt', 'utf8', function (err, data) {
+fs.readFile('readme.txt', 'utf8', function(err, data) {
     if (err) throw err;
 
     console.log(data);
 });
 console.log('!!!');
- */
+ 
 
